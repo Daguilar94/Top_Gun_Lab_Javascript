8f9a5efb87847c27818db9e79322c2aa6878709d
David Aguilar <davidaguilarh.94@gmail.com>
2019-05-10 23:16:26 -0500

    Refactor loop exercises

---
+++ ejercicios/ciclos/for.js
@@ -0,0 +1,10 @@
+// Realiza el siguiente ejercicio usando (for).
+
+// 1. Utiliza el ciclo for para encontrar el valor promedio entre los enteros
+// contenidos en el siguiente arreglo:
+
+var natural = [1, 50, 34, 289, 32, 65, 76, 89, 32, 5, 12];
+
+
+// ***** Para correr este archivo en la consola de node ejecuta el siguiente comando:
+// ***** node ./ejercicios/ciclos/for.js
--- js/ciclos/question.js
+++
@@ -1,3 +0,0 @@
-// 1. Encuentra el valor promedio entre los enteros contenidos en el arreglo natural.
-
-var natural = [1, 50, 34, 289, 32, 65, 76, 89, 32, 5, 12];
