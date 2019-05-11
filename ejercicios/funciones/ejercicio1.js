181d769e8a8788d0ba100881477e700bcfa4d486
David Aguilar <davidaguilarh.94@gmail.com>
2019-05-11 00:15:43 -0500

    Refactor functions exercises

---
+++ ejercicios/funciones/ejercicio1.js
@@ -0,0 +1,11 @@
+// 1. Crea una funcion que reciba un número como argumento y lo eleve al cuadrado.
+
+
+
+// crea una función que reciba un número (n) y una función
+// similar a la creada en el punto anterior (f) como argumentos y
+// retorne el resultado de la ejecución de la función (f) con el número (n) pasado como argumento.
+
+
+// ***** Para correr este archivo en la consola de node ejecuta el siguiente comando:
+// ***** node ./ejercicios/funciones/ejercicio1.js
---
+++ ejercicios/funciones/ejercicio2.js
@@ -0,0 +1,7 @@
+// 1. Escriba una función que reciba un string como argumento y retorne el string
+// con la primera letra de cada una de sus palabras en mayúscula.
+// elemplo: f("hola como estás?") = "Hola Como Estas?"
+
+
+// ***** Para correr este archivo en la consola de node ejecuta el siguiente comando:
+// ***** node ./ejercicios/funciones/ejercicio2.js
