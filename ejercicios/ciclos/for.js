7c8c7ac523f894aabeac6985500b43a732434f0e
David Aguilar <davidaguilarh.94@gmail.com>
2019-05-10 23:24:55 -0500

    Improve for exercise

--- ejercicios/ciclos/for.js
+++ ejercicios/ciclos/for.js
@@ -1,6 +1,8 @@
 // Realiza el siguiente ejercicio usando (for).
 
-// 1. Utiliza el ciclo for para encontrar el valor promedio entre los enteros
+// 1. Se da un arreglo con las edades de las personas que van a asistir a un concierto y se necesita
+// encontrar la edad promedio de los asistentes.
+// Utiliza el ciclo for para encontrar el valor promedio de los enteros
 // contenidos en el siguiente arreglo:
 
 var natural = [1, 50, 34, 289, 32, 65, 76, 89, 32, 5, 12];
