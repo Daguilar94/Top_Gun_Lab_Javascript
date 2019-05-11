d806d9e3b8420791c7f9ba401566551b5a577166
David Aguilar <davidaguilarh.94@gmail.com>
2019-05-11 00:50:47 -0500

    Modularize js solution

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+import {
+    getPassengersInfo,
+    getAverageAge,
+    getTotalProfit
+} from "../practica/ejercicios.js";
+
 (function() {
     "use strict"
     let passengers = [
@@ -95,82 +101,4 @@
     updatePassengers();
     updateAverageAge();
     updateTotalProfit();
-})()
-
-
-// -----------------------------
-// 1. Crea una función llamada "getPassengersInfo" que reciba un número entero (x), 
-// debe preguntar al usuario por el nombre y edad de la persona a inscribir (x) veces.
-// se debe construir un arreglo de objetos con la información recogida. Cada objeto corresponde
-// a un tripulante de modo que el objeto retornado debe tener la siguiente estuctura:
-
-var returnedArray = [
-    { name: '...', age: '...' },
-    { name: '...', age: '...' },
-    { name: '...', age: '...' }
-];
-
-const getPassengersInfo = (passengersAmount) => {
-    let passengers = [];
-    for(let i = 0; i < passengersAmount; i++) {
-        const name = prompt('Ingresa tu nombre');
-        let age = prompt('Ingresa tu edad');
-        age = parseInt(age, 10);
-        passengers.push({ name, age });
-    }
-    return passengers;
-};
-//------------------------------
-// -----------------------------
-// 2.  Crea una función llamada "getAverageAge" que reciba un arreglo de objetos
-// con la siguiente estructura:
-
-var passengers = [
-    { name: '...', age: '...' },
-    { name: '...', age: '...' },
-    { name: '...', age: '...' }
-];
-
-// Y que retorne el valor promedio de los valores de la llave age en los objetos.
-
-function getAverageAge(passengers) {
-    const sum = passengers.reduce((acc, passenger) => acc += passenger.age, 0);
-    const passengersAmount = passengers.length;
-    return sum/passengersAmount;
-};
-//------------------------------
-// -----------------------------
-// 3. Crea una función llamada "getTotalProfit" que reciba un arreglo de objetos
-// con la siguiente estructura:
-
-var passengers = [
-    { name: '...', age: '...' },
-    { name: '...', age: '...' },
-    { name: '...', age: '...' }
-];
-
-// Y de acuerdo con la siguiente tabla de precio según edad, retorne la suma total de
-// cada uno de los tripulantes
-
-// Personas de 13 años o menores                        3000 dólares
-// Personas de 13 hasta 30 años (incluyendo el 30)      5000 dólares
-// Personas mayores de 30 años                          4000 dólares
-
-function getTotalProfit(passengers) {
-    let amounts = passengers.map(passenger => {
-        const age = passenger.age;
-        switch(true) {
-            case (age <= 13):
-                return 3000;
-            case (age > 13 && age <= 30):
-                return 5000
-            case (age > 30):
-                return 4000
-            default:
-                return 0;
-        }
-    });
-    const sum = amounts.reduce((acc, amount) => acc += amount, 0);
-    return sum
-};
-//------------------------------
+})();
---
+++ practica/ejercicios.js
@@ -0,0 +1,78 @@
+// -----------------------------
+// 1. Crea y exporta una función llamada "getPassengersInfo" que reciba un número entero (x), 
+// debe preguntar al usuario por el nombre y edad de la persona a inscribir (x) veces.
+// se debe construir un arreglo de objetos con la información recogida. Cada objeto corresponde
+// a un tripulante de modo que el objeto retornado debe tener la siguiente estuctura:
+
+[
+    { name: '...', age: 5 },
+    { name: '...', age: 23 },
+    { name: '...', age: 2 }
+]
+
+export const getPassengersInfo = (passengersAmount) => {
+    let passengers = [];
+    for(let i = 0; i < passengersAmount; i++) {
+        const name = prompt('Ingresa tu nombre');
+        let age = prompt('Ingresa tu edad');
+        age = parseInt(age, 10);
+        passengers.push({ name, age });
+    }
+    return passengers;
+};
+
+// -----------------------------
+// 2.  Crea y exporta una función llamada "getAverageAge" que reciba un arreglo de objetos
+// con la siguiente estructura:
+
+[
+    { name: '...', age: 5 },
+    { name: '...', age: 23 },
+    { name: '...', age: 2 }
+]
+
+// Y que retorne el valor promedio de los valores de la llave age en los objetos.
+
+export function getAverageAge(passengers) {
+    const sum = passengers.reduce((acc, passenger) => acc += passenger.age, 0);
+    const passengersAmount = passengers.length;
+    return sum / passengersAmount;
+};
+
+// -----------------------------
+// 3. Crea y exporta una función llamada "getTotalProfit" que reciba un arreglo de objetos
+// con la siguiente estructura:
+
+[
+    { name: '...', age: 5 },
+    { name: '...', age: 23 },
+    { name: '...', age: 2 }
+]
+
+// Y de acuerdo con la siguiente tabla de precio según edad, retorne la suma total de
+// cada uno de los tripulantes
+
+// Personas de 13 años o menores                        3000 dólares
+// Personas de 13 hasta 30 años (incluyendo el 30)      5000 dólares
+// Personas mayores de 30 años                          4000 dólares
+
+
+export function getTotalProfit(passengers) {
+    let amounts = passengers.map(passenger => {
+        const age = passenger.age;
+        switch(true) {
+            case (age <= 13):
+                return 3000;
+            case (age > 13 && age <= 30):
+                return 5000
+            case (age > 30):
+                return 4000
+            default:
+                return 0;
+        }
+    });
+    const sum = amounts.reduce((acc, amount) => acc += amount, 0);
+    return sum
+};
+
+//------------------------------
