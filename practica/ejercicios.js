49d7d1b698d6b59ead45924aaa36554856449e30
David Aguilar <davidaguilarh.94@gmail.com>
2019-05-13 23:23:22 -0500

    Improve practice description

--- practica/ejercicios.js
+++ practica/ejercicios.js
@@ -1,19 +1,26 @@
 // -----------------------------
 // 1. Crea y exporta una función llamada "getPassengersInfo" que reciba un número entero (x), 
-// debe preguntar al usuario por el nombre y edad de la persona a inscribir (x) veces.
+// debe preguntar al usuario por el nombre y edad de la persona a inscribir (x) veces
+// (usando window.prompt()).
+// Como si estuviera preguntando por la información de varias personas, es decir,
+// Se pregunta por el primer nombre, luego por la edad de esa primera persona.
+// Una vez la primera persona ingrese la información, se procede a preguntar el nombre
+// y la edad de la segunda y así (x) veces.
 // se debe construir un arreglo de objetos con la información recogida. Cada objeto corresponde
-// a un tripulante de modo que el objeto retornado debe tener la siguiente estuctura:
+// a la información de una persona, de modo que el objeto retornado por la función
+// debe tener la siguiente estuctura:
+
+[
+    { name: '...', age: 5 }, 
+    { name: '...', age: 23 },
+    { name: '...', age: 2 }
+]
+// Observa que las edades son números, no strings, usa parseInt() para hacer la conversión.
 
 // PISTA: para exportar una función lo puedes hacer de las siguientes maneras:
 // 1. export function miFuncion() {...}
 // O si usas arrow functions, así:
 // 2. export const miFuncion = () => {...}
-
-[
-    { name: '...', age: 5 },
-    { name: '...', age: 23 },
-    { name: '...', age: 2 }
-]
 
 
 
@@ -47,8 +54,8 @@
     { name: '...', age: 2 }
 ]
 
-// Y de acuerdo con la siguiente tabla de precio según edad, retorne la suma total de
-// cada uno de los tripulantes
+// Y de acuerdo con la siguiente tabla de precios según edad, retorne la suma total del
+// dinero entregado por cada una de las personas.
 
 // Personas de 13 años o menores                        3000 dólares
 // Personas de 13 hasta 30 años (incluyendo el 30)      5000 dólares
