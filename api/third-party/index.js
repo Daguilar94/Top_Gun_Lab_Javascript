72f688b21bba77f6372923f2118c6a301ad9daae
David Aguilar <davidaguilarh.94@gmail.com>
2019-05-18 02:10:03 -0500

    Remove third party solutions

--- api/third-party/index.js
+++ api/third-party/index.js
@@ -3,23 +3,23 @@
 // la siguiente url: https://rickandmortyapi.com/documentation.
 // Muestra en consola el json que se obtiene como respuesta.
 
-fetch("https://rickandmortyapi.com/api/character/")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        console.log(response);
-        const characters = response.results;
-        const imageArray = createImageArray(characters);
-        loadImages(imageArray);
-    })
-    .catch(function(error) {
-        console.log("Error: " + error);
-    })
+
+
+
+
+
+
+
 
 // 2. Obtener el contenedor de los personajes.
 
-const characterContainer = document.getElementById("character-container");
+
+
+
+
+
+
+
 
 // 3. Crear una funcion llamada createImage que reciba como parámetro una url,
 // la función deberá crear una imagen y a está se le debe agreguar la clase
@@ -27,13 +27,15 @@ const characterContainer = document.getElementById("character-container");
 // recibido en el parámetro.
 // se debe retornar la imagen creada.
 
-function createImage(src) {
-    let avatar = document.createElement("img");
-    avatar.classList.add("avatar-image");
-    avatar.alt = "avatar";
-    avatar.src = src;
-    return avatar;
-}
+
+
+
+
+
+
+
+
+
 
 // 4. Crear una función llamada createImageArray, que recibe un arreglo de personajes con
 // mucha información y lo que debe hacer es recorrer este arreglo y para cara elemento del arreglo
@@ -42,20 +44,24 @@ function createImage(src) {
 // Para saber cual es el valor que se debe pasar debes revisar la respuesta de la API y encontrar
 // que valor del json corresponde a la src de la foto del personaje.
 
-function createImageArray(charactersArray) {
-    return charactersArray.map(function(character) {
-        return createImage(character.image);
-    })
-}
+
+
+
+
+
+
+
+
 
 // 5. Crear una función llamada loadImages la cual reciba un arreglo de elementos (img) de html y
 // agregue cada una de estas como hijos del elemento contenedor de los personajes.
 
-function loadImages(imageArray) {
-    imageArray.forEach(function(image) {
-        characterContainer.appendChild(image);
-    })
-}
+
+
+
+
+
+
 
 // 6. En el punto 1 donde obtuviste la respuesta del servidor (dentro del segundo then), utiliza la
 // función createImageArray para crear el arreglo de imágenes a partir de la respuesta
